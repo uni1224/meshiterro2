f2737cd88d8d8a772d211dedc63126d484bf8bff
uni1224 <saekahane99@gmail.com>
2023-08-28 10:07:22 +0000

    バリデーション・いいね

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,16 @@ class PostImage < ApplicationRecord
     belongs_to :user
     
     has_many :post_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    
+    validates :shop_name, presence: true
+    validates :image, presence: true
+    
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end
+    
+        
     
     def get_image
     unless image.attached?
