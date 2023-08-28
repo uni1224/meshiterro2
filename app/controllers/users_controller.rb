848a3183f67c8e43c8ee50dfbb80284e8bebe209
uni1224 <saekahane99@gmail.com>
2023-08-28 12:49:46 +0000

    devise設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
